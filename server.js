78816e5f6e3a4373b6ebd8df02e7d08bf944e5ba
Ronald K <36178896+ronCodeTracker@users.noreply.github.com>
2025-06-06 10:32:30 -0500

    preflight cors mod

--- server.js
+++ server.js
@@ -12,10 +12,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import videoRoutes from './routes/videoRoutes.js';
+import cors from 'cors';
+
+
+
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', videoRoutes);
